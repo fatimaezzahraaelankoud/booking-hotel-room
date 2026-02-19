423dd2ec4ead32fca7deff16ae852b0f08c9413d
fatimaezzahraaelankoud <fatimaezzahraaelankoud@gmail.com>
2026-02-19 12:22:00 +0100

    Initial commit - Spring Boot hotel booking project

---
+++ src/main/java/com/fatidev/fatihotel/FatihotelApplication.java
@@ -0,0 +1,13 @@
+package com.fatidev.fatihotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatihotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatihotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatidev/fatihotel/FatihotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatidev.fatihotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatihotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
